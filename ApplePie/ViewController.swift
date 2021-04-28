59a4890e7b7eed3eb396a60eaae73e4ee949e022
snamiki1212 <snamiki1212@gmail.com>
2021-04-28 15:54:40 -0700

    Update didSet on win/lose

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -23,14 +23,33 @@ class ViewController: UIViewController {
         "program"
     ]
     let incorrectMovesAllowed = 7
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     var currentGame: Game!
     
     func newRound(){
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessesLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessesLetters: [])
+            enableLettersButtons(true)
+            updateUI()
+        } else {
+            enableLettersButtons(false)
+        }
+    }
+    
+    func enableLettersButtons(_ enable: Bool){
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
     }
     
     func updateUI(){
@@ -47,6 +66,17 @@ class ViewController: UIViewController {
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
     
+    func updateGameState(){
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+        
+    }
+    
     @IBOutlet var letterButtons: [UIButton]!
     
     @IBAction func letterButtonPressed(_ sender: UIButton) {
@@ -54,7 +84,7 @@ class ViewController: UIViewController {
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+        updateGameState()
     }
     
     @IBOutlet var treeImageView: UIImageView!
