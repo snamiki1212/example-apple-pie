a64254cbeac5f996cea0b280a8df8c8f81d7ac96
snamiki1212 <snamiki1212@gmail.com>
2021-04-28 14:04:04 -0700

    Update to connect button to outlet

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBOutlet var treeImageView: UIImageView!
+    
+    @IBOutlet var correctWordLabel: UILabel!
 }
 
